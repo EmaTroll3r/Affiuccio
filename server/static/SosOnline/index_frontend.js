61d0ee77887b901ff7137fc1c56569ec677da390
Ema <emanuelecostanzo03@gmail.com>
2024-07-22 11:49:30 +0200

    resetted timer when vibility changes

--- server/static/SosOnline/index_frontend.js
+++ server/static/SosOnline/index_frontend.js
@@ -1,12 +1,26 @@
 
+let timer;
+let shackeTimer;
+
 function shakeScreen(time){
     shakeableIndex.classList.add('shake');
-    setTimeout(function() {
+    shackeTimer = setTimeout(function() {
         shakeableIndex.classList.remove('shake');
     }, time);
 }
+function startTimer(){
+    clearTimeout(timer);
+    shakeableIndex.classList.remove('shake');
+    timer = setTimeout(function() {
+        shakeScreen(100000);
+    }, 12000); // 12000 millisecondi equivalgono a 12 secondi
+}
 
-setTimeout(function() {
-    shakeScreen(100000);
-}, 12000); // 12000 millisecondi equivalgono a 12 secondi
+startTimer();
 
+document.addEventListener('visibilitychange', function() {
+    if (!document.hidden) {
+        startTimer();
+        //console.log("timer resetted")
+    }
+});
