1b64aabc80c9d76e739f56e785314ab2c785d8a4
Ema <emanuelecostanzo03@gmail.com>
2024-09-26 16:38:37 +0200

    try avoid shutdown after error

--- run.py
+++ run.py
@@ -25,5 +25,8 @@ if __name__ == '__main__':
     #Only need for test in server
     elif test == False:
         context = ('/etc/letsencrypt/live/affiuccio.duckdns.org/fullchain.pem', '/etc/letsencrypt/live/affiuccio.duckdns.org/privkey.pem')
-        socketio.run(app, host='0.0.0.0', port=443, ssl_context=context)
+        try:
+            socketio.run(app, host='0.0.0.0', port=443, ssl_context=context)
+        except Exception as e:
+            pass
 
