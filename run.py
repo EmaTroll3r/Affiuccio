3050a12fd9fae6f2bca826dfd58d48c90a110e56
Ema <emanuelecostanzo03@gmail.com>
2024-09-29 15:55:04 +0200

    add general try except

--- run.py
+++ run.py
@@ -1,32 +1,34 @@
+#https://affiuccio.duckdns.org/SosOnline
 from server import create_app
 from global_vars import socketio, test
 from flask_cors import CORS
 from server.garbageColletor import garbageCollector
 
 
+try:
 
-app = create_app()
-CORS(app)
+    app = create_app()
+    CORS(app)
 
-#socketio = SocketIO(app, cors_allowed_origins="*")
+    #socketio = SocketIO(app, cors_allowed_origins="*")
 
-socketio.init_app(app)
+    socketio.init_app(app)
 
-if not test:
-    garbageCollector()
+    if not test:
+        garbageCollector()
 
 
-#"""
-if __name__ == '__main__':
-    #Only need for test in local
-    if test == True:
-        socketio.run(app, host='0.0.0.0', port=80)
+    #"""
+    if __name__ == '__main__':
+        #Only need for test in local
+        if test == True:
+            socketio.run(app, host='0.0.0.0', port=80)
 
-    #Only need for test in server
-    elif test == False:
-        context = ('/etc/letsencrypt/live/affiuccio.duckdns.org/fullchain.pem', '/etc/letsencrypt/live/affiuccio.duckdns.org/privkey.pem')
-        try:
+        #Only need for test in server
+        elif test == False:
+            context = ('/etc/letsencrypt/live/affiuccio.duckdns.org/fullchain.pem', '/etc/letsencrypt/live/affiuccio.duckdns.org/privkey.pem')
             socketio.run(app, host='0.0.0.0', port=443, ssl_context=context)
-        except Exception as e:
-            pass
+            
+except Exception as e:
+    pass   
 
