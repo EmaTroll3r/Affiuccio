db46acc3395debce38a41145213166cba86cbd44
Ema <emanuelecostanzo03@gmail.com>
2024-11-27 21:23:17 +0100

    commit

--- server/apps/SyncWatch/routes.py
+++ server/apps/SyncWatch/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from global_vars import partyManager,socketio, test
+from flask import jsonify
 #from server.apps import SosOnline as sosOnline
 #from json import load
 
@@ -8,5 +9,5 @@ syncwatch = Blueprint('syncwatch', __name__)
 
 @syncwatch.route('/create-party', methods=['GET'])
 def syncwatch_create_party():
-    #partyID = partyManager.create_party()
-    return 7#partyID
+    partyID = partyManager.create_party()
+    return jsonify({'partyID': partyID})  # Restituisce un dizionario JSON
