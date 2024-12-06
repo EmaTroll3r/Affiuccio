335d11cbb59262ed518152a843c473db9daff51c
Ema <emanuelecostanzo03@gmail.com>
2024-12-06 21:44:18 +0100

    final version SyncWatch

--- server/apps/SyncWatch/functions.py
+++ server/apps/SyncWatch/functions.py
@@ -3,25 +3,30 @@ import random
 
 parties = {}
 
-def syncwatch_create_party():
+def syncwatch_create_party(url=""):
     while True:
-        #party_id = random.randint(1000, 9999)
-        party_id = 2
-        #if party_id not in parties:
-        syncwatch_add_party(party_id)
+        party_id = random.randint(1000, 9999)
+        #party_id = 2
+        if party_id not in parties:
+            syncwatch_add_party(party_id,url)
         return party_id
 
-def syncwatch_add_party(partyID):
+def syncwatch_add_party(partyID,url=""):
     #global parties
-    parties[partyID] = {"play_pause": 0, "next": 0, "previous": 0, "play_pause_state": "play"}
+    parties[partyID] = {"play_pause": 0,"curr_time":0, "play_pause_state": "pause","url": url}
     print("\n\n party added:", partyID,"\n\n")
     return partyID
+
+def syncwatch_add_url(partyID, url):
+    parties[partyID]["url"] = url
+    return url
 
 def syncwatch_get_current_values(partyID):
     #global pause, next, previous, play
     print(parties[partyID])
     return parties[partyID]
 
+"""
 def syncwatch_next(partyID):
     #global parties
     parties[partyID]["next"] += 1
@@ -32,7 +37,7 @@ def syncwatch_previous(partyID):
     #previous += 1
     parties[partyID]["previous"] += 1
     return parties[partyID]["previous"]
-
+"""
 
 def syncwatch_play_pause(partyID):
     parties[partyID]["play_pause"] += 1
@@ -45,3 +50,7 @@ def syncwatch_play_pause_state(partyID):
     else:
         parties[partyID]["play_pause_state"] = "play"
     return parties[partyID]["play_pause_state"]
+
+def syncwatch_set_time(partyID, time):
+    parties[partyID]["curr_time"] = time
+    return time
--- server/apps/SyncWatch/routes.py
+++ server/apps/SyncWatch/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request
-from global_vars import partyManager,socketio, test
-from flask import jsonify
-from server.classes import Party
+from flask import Blueprint, request
 from server.apps import SyncWatch as syncWatch
+from flask import jsonify
+
+#from global_vars import partyManager,socketio, test
+#from server.classes import Party
 #from json import load
 
 syncwatch = Blueprint('syncwatch', __name__)
@@ -10,9 +11,18 @@ syncwatch = Blueprint('syncwatch', __name__)
 
 @syncwatch.route('/create-party', methods=['GET'])
 def syncwatch_create_party():
-    #partyID = Party.create_party('SyncWatch',test=test)
     partyID = syncWatch.syncwatch_create_party()
-    return jsonify({"result": partyID,"state":0, "error": "no error"})
+    return jsonify({"response": partyID,"state":0, "error": "no error"})
+
+@syncwatch.route('/create-party-url', methods=['POST'])
+def syncwatch_create_party_with_url():
+    data = request.get_json()
+    try:
+        url = data.get('url', 'default_value')
+    except:
+        return jsonify({"response": 0,"state":1,"error": "Invalid url"})
+    partyID = syncWatch.syncwatch_create_party(url=url)
+    return jsonify({"response": partyID,"state":0, "error": "no error"})
 
 
 @syncwatch.route('/play-pause', methods=['POST'])
@@ -21,19 +31,20 @@ def syncwatch_play_pause_endpoint():
     try:
         partyID = int(data.get('partyID', 'default_value'))
     except:
-        return jsonify({"result": 0,"state":1,"error": "Invalid partyID"})
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
     play_pause = syncWatch.syncwatch_play_pause(partyID)
-    return jsonify({"result": play_pause,"state":0,"error": "no error"})
+    return jsonify({"response": play_pause,"state":0,"error": "no error"})
 
+"""
 @syncwatch.route('/next', methods=['POST'])
 def syncwatch_next_endpoint():
     data = request.get_json()
     try:
         partyID = int(data.get('partyID', 'default_value'))
     except:
-        return jsonify({"result": 0,"state":1,"error": "Invalid partyID"})
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
     next = syncWatch.syncwatch_next(partyID)
-    return jsonify({"result": next,"state":0,"error": "no error"})
+    return jsonify({"response": next,"state":0,"error": "no error"})
 
 @syncwatch.route('/previous', methods=['POST'])
 def syncwatch_previous_endpoint():
@@ -41,14 +52,38 @@ def syncwatch_previous_endpoint():
     try:
         partyID = int(data.get('partyID', 'default_value'))
     except:
-        return jsonify({"result": 0,"state":1,"error": "Invalid partyID"})
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
     previous = syncWatch.syncwatch_previous(partyID)
-    return jsonify({"result": previous,"state":0,"error": "no error"})
+    return jsonify({"response": previous,"state":0,"error": "no error"})
+"""
+    
 
 @syncwatch.route('/get-current-values', methods=['POST'])
 def syncwatch_get_current_values_endpoint():
     try:
         partyID = int(request.get_json().get('partyID', 'default_value'))
     except:
-        return jsonify({"result": 0,"state":1,"error": "Invalid partyID"})
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
     return jsonify(syncWatch.syncwatch_get_current_values(partyID))
+
+@syncwatch.route('/set-time', methods=['POST'])
+def syncwatch_set_time_endpoint():
+    data = request.get_json()
+    try:
+        partyID = int(data.get('partyID', 'default_value'))
+        time = int(data.get('time', 'default_value'))
+    except:
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
+    time = syncWatch.syncwatch_set_time(partyID,time)
+    return jsonify({"response": time,"state":0,"error": "no error"})
+
+@syncwatch.route('/add-url', methods=['POST'])
+def syncwatch_add_url_endpoint():
+    data = request.get_json()
+    try:
+        partyID = int(data.get('partyID', 'default_value'))
+        url = int(data.get('url', 'default_value'))
+    except:
+        return jsonify({"response": 0,"state":1,"error": "Invalid partyID"})
+    url = syncWatch.syncwatch_add_url(partyID,url)
+    return jsonify({"response": url,"state":0,"error": "no error"})
