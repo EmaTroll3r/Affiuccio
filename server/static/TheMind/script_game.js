9fbd0247721f82c9efed51fa4b4a18e180fa3a8e
EmaTroll3r <emanuelecostanzo03@gmail.com>
2026-04-19 14:08:57 +0200

    reduced vibration time

--- server/static/TheMind/script_game.js
+++ server/static/TheMind/script_game.js
@@ -161,7 +161,7 @@ function playedCardAnimation(card) {
 }
 
 socket.on('next-level', async function(data) {
-    vibrateDevice(500);
+    vibrateDevice(400);
     await alert('Congratulations! You have reached level ' + data.level, 0, 'Level Up!');
     levelElement.setAttribute('data-text', data.level.toString());
 
