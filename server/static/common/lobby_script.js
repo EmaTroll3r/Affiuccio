df0fea08dd171c064eaf9838217f12af66e676be
EmaTroll3r <emanuelecostanzo03@gmail.com>
2026-04-23 14:04:03 +0200

    try fix share icon

--- server/static/common/lobby_script.js
+++ server/static/common/lobby_script.js
@@ -212,7 +212,7 @@ document.getElementById('invite-player').addEventListener('click', async () => {
                             const drawX = (size - drawWidth) / 2;
                             const drawY = (size - drawHeight) / 2;
 
-                            ctx.drawImage(img, 0, 0, 1200, 1200);
+                            ctx.drawImage(img, 0, 0, 50, 50);
 
                             const outBlob = await new Promise((resolve) => {
                                 canvas.toBlob(resolve, 'image/jpeg', 0.92);
